43a93a9923cf9aaf5280ec0f23e8eee2865e3155
yu-M215 <yuu0609km.215@gmail.com>
2022-02-28 01:47:35 +0900

    [Chapter4] Javaにおける変数と定数

---
+++ wcp/JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		// 自分の名前を「String」型の変数に代入して出力
+		String str = "ゆう";
+		System.out.println(str);
+		// 好きな言葉を同じ変数に再代入して出力
+		str = "花鳥風月";
+		System.out.println(str);
+		// ラッキーナンバーとして「int」型で宣言した定数を出力
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
