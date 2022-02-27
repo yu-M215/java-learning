7d198a32f7d3a24f920b935ed24dab5a4ddc80e7
yu-M215 <yuu0609km.215@gmail.com>
2022-02-28 01:02:18 +0900

    [Chapter3] System.out.printlnで文字列の標準出力

--- wcp/JavaWcp/src/Chapter02.java
+++ wcp/JavaWcp/src/Chapter02.java
@@ -1,6 +1,8 @@
 
 public class Chapter02 {
 	public static void main(String[] args) {
-		System.out.println("こんにちは！");
+//		System.out.println("こんにちは！");
+		System.out.println("Hello WEBCAMP!");
+		System.out.println("Hello Java!");
 	}
 }
