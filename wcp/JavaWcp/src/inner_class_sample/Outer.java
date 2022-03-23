23f2696c2e2b595d2d5f5b4299a8fdb917bc8625
yu-M215 <yuu0609km.215@gmail.com>
2022-03-23 18:43:22 +0900

    [ExChapter01] 外部クラスのフィールドを内部クラスから参照する

--- wcp/JavaWcp/src/inner_class_sample/Outer.java
+++ wcp/JavaWcp/src/inner_class_sample/Outer.java
@@ -1,6 +1,12 @@
 package inner_class_sample;
 
 public class Outer {
-	public static class Inner {
+	// 外部クラスにに定義したフィールドgreeting
+	private String greeting = "こんにちは";
+	public class Inner {
+		public String hello() {
+			// 内部クラスInnerから外部クラスのOuterクラスのフィールドgreetingを参照
+			return greeting;
+		}
 	}
 }
