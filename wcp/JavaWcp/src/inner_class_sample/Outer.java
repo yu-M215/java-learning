6a3503a196af0bef00d297ac780dfec5686dd557
yu-M215 <yuu0609km.215@gmail.com>
2022-03-23 18:45:42 +0900

    [ExChapter01] staticなフィールドならstaticな内部クラスから参照可能

--- wcp/JavaWcp/src/inner_class_sample/Outer.java
+++ wcp/JavaWcp/src/inner_class_sample/Outer.java
@@ -2,8 +2,8 @@ package inner_class_sample;
 
 public class Outer {
 	// 外部クラスにに定義したフィールドgreeting
-	private String greeting = "こんにちは";
-	public class Inner {
+	private static String greeting = "こんにちは";
+	public static class Inner {
 		public String hello() {
 			// 内部クラスInnerから外部クラスのOuterクラスのフィールドgreetingを参照
 			return greeting;
